11b1b15d9a9ec952f8d697a875431af293540383
Laerti <laerti.alla@gmail.com>
2024-03-25 17:07:48 +0100

    Models

---
+++ HumanResourceProject/Models/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HumanResourceProject.Models
+{
+    public partial class Notification
+    {
+        public int NotificationId { get; set; }
+        public int SenderClientId { get; set; }
+        public int ReceiverClientId { get; set; }
+        public bool IsRead { get; set; }
+        public int ReservationId { get; set; }
+
+        public virtual Client ReceiverClient { get; set; } = null!;
+        public virtual Reservation Reservation { get; set; } = null!;
+        public virtual Client SenderClient { get; set; } = null!;
+    }
+}
---
+++ HumanResourceProject/Models/ReservationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HumanResourceProject.Models
+{
+    public partial class ReservationService
+    {
+        public int ReservationServicesId { get; set; }
+        public int ReservationId { get; set; }
+        public int ServicesId { get; set; }
+
+        public virtual Reservation Reservation { get; set; } = null!;
+        public virtual ExtraService Services { get; set; } = null!;
+    }
+}
