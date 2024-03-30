672fa26593f1104d7362da41bd9a95429f249370
Laerti <laerti.alla@gmail.com>
2024-03-30 17:10:24 +0100

    DAL

---
+++ DAL/Concrete/ExtraServicesRepository.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using DAL.Contracts;
+using DTO.ExtraServiceDTO;
+using HumanResourceProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Concrete
+{
+    public class ExtraServicesRepository : IExtraServicesRepository
+    {
+        private readonly HospitalityPRO_DbContext _dbContext;
+        private readonly IMapper _mapper;
+        public ExtraServicesRepository(HospitalityPRO_DbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<ExtraService>> GetAllServices()
+        {
+            return await _dbContext.ExtraServices.ToListAsync();
+        }
+        public async Task<ExtraService> GetExtraServicesById(int serviceId)
+        {
+            return await _dbContext.ExtraServices.FirstOrDefaultAsync(c => c.ServicesId == serviceId)
+                ?? throw new Exception("Not Found!!!");
+        }
+        public async Task<ExtraService> AddExtraService(ExtraServiceDTO request)
+        {
+            var extraService = new ExtraService
+            {
+                Type = request.Type,
+                Price = request.Price,
+                Description = request.Description
+            };
+
+            var addedExtraService = _dbContext.ExtraServices.Add(extraService);
+            await _dbContext.SaveChangesAsync();
+
+            return addedExtraService.Entity;
+        }
+
+    }
+}
---
+++ DAL/Contracts/IExtraServicesRepository.cs
@@ -0,0 +1,18 @@
+﻿using DTO.ExtraServiceDTO;
+using HumanResourceProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Contracts
+{
+    public interface IExtraServicesRepository
+    {
+        Task<ExtraService> AddExtraService(ExtraServiceDTO request);
+        Task<IEnumerable<ExtraService>> GetAllServices();
+        Task<ExtraService> GetExtraServicesById(int serviceId);
+
+    }
+}
