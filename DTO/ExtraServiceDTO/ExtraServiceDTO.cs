635453f22508131952e5fe65d9832b0fd29edcff
Laerti <laerti.alla@gmail.com>
2024-03-31 13:20:50 +0200

    DTO updated

--- DTO/ExtraServiceDTO/ExtraServiceDTO.cs
+++ DTO/ExtraServiceDTO/ExtraServiceDTO.cs
@@ -8,6 +8,7 @@ namespace DTO.ExtraServiceDTO
 {
     public class ExtraServiceDTO
     {
+       public int ServiceId { get; set; }
         public string Type { get; set; } = null!;
         public double Price { get; set; }
         public string Description { get; set; } = null!;
